e10439f46964d00cba90b2d9e51e7879241f227b
Rishabh Agrawal <rishabh0148@email.com>
2018-10-06 21:40:11 -0500

    --Initialize Android Project files --Test Code --Integrate Speech Recognition Github Library --Onboarding Libraries and tutorial --Setup Launcher Intent --Add Service class for the project

--- appmodule/src/main/java/winnerteam/hackuta/appmodule/MainActivity.java
+++ appmodule/src/main/java/winnerteam/hackuta/appmodule/MainActivity.java
@@ -3,11 +3,13 @@ package winnerteam.hackuta.appmodule;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    public static final String TAG=MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +20,16 @@ public class MainActivity extends AppCompatActivity {
     public void StartNow(View view){
         Intent intent=new Intent(this, ListeningActivity.class);
         startActivity(intent);
-
+        populateCharacters();
 
     }
+
+    private void populateCharacters() {
+        Log.i(TAG, "populateCharacters: ");
+
+        Character
+
+    }
+
+
 }
